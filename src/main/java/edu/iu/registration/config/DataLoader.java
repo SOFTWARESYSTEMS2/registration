49a02bb6187b7ad7d75095120b25cd0a7da8cad3
Jetson Black <jetsondblack@gmail.com>
2026-04-08 15:08:45 -0400

    .

--- src/main/java/edu/iu/registration/config/DataLoader.java
+++ src/main/java/edu/iu/registration/config/DataLoader.java
@@ -81,7 +81,7 @@ public class DataLoader {
                     "user",
                     "userpass",
                     "STUDENT",
-                    "Senior",
+                    "Sophomore",
                     csMajor,
                     mathMinor,
                     softwareEng
